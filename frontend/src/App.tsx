355544aba2cc3932e7400eee5540abe13d994eba
JNVCPRASAD1507 <chandraprasadkolli@gmail.com>
2026-09-08 10:56:02 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+    </>
+  )
+}
+
+export default App
